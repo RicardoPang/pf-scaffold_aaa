85bc0584c6ffc19bb518f1f94206587481711af4
pangfeng <ricardo.pangj@gmail.com>
2024-10-14 22:50:21 +0800

    test

--- examples/vue.config.js
+++ examples/vue.config.js
@@ -5,5 +5,9 @@ module.exports = {
       entry: './src/main.js',
       template: 'public/index.html',
     },
+    index2: {
+      entry: './src/main.js',
+      template: 'public/index.html',
+    },
   },
 };
